d201d81b13a13e88cd04cce1d910c5973400b425
lillywolf <lillywolf@gmail.com>
2010-03-22 12:45:38 -0700

    test

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -15,8 +15,8 @@ class UserController < ApplicationController
     array = Array.new
     hash = Hash.new
     user = User.find(params[:id])
-    user.credits = user.credits + params[:to_add]
-    user.save
+    # user.credits = user.credits + params[:to_add]
+    # user.save
     hash["instance"] = user
     hash["model"] = "user"
     array.push hash
