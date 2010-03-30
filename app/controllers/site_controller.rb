63658d95d5de6c563133e27b135495d6478006e2
lillywolf <lillywolf@gmail.com>
2010-03-29 23:00:04 -0700

    small changes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -64,7 +64,7 @@ class SiteController < ApplicationController
   end  
   
   def get_facebook_friend_data
-    ensure_application_is_installed_by_facebook_user        
+    # ensure_application_is_installed_by_facebook_user        
     array = Array.new
     # user = setup_facebook_user  
     # if ( user.name rescue false )
