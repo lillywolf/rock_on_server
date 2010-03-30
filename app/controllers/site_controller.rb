dd70535ee7df7ddd3cac51f9fc2210b9da5002db
lillywolf <lillywolf@gmail.com>
2010-03-29 23:14:47 -0700

    small changes

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -70,9 +70,9 @@ class SiteController < ApplicationController
       render :text => 'Where are you?'
     else
       friends = user.friends
-      # friends.each do |f|
+      friends.each do |f|
         render :text => user.name
-      # end  
+      end  
     end
     # render :json => array.to_json        
   end  
