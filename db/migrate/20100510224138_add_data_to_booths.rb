dbc2d43e48faf9b2b2952562e541821a7c57c800
lillywolf <lillywolf@gmail.com>
2010-05-20 11:55:32 -0700

    heroku copy

--- db/migrate/20100510224138_add_data_to_booths.rb
+++ db/migrate/20100510224138_add_data_to_booths.rb
@@ -1,13 +1,13 @@
 class AddDataToBooths < ActiveRecord::Migration
   def self.up
-    add_column :booth_structures, :structure_id, :int
-    add_column :booth_structures, :inventory_capacity, :int
-    add_column :booth_structures, :item_price, :int
+    add_column :booths, :structure_id, :int
+    add_column :booths, :inventory_capacity, :int
+    add_column :booths, :item_price, :int
   end
 
   def self.down
-    remove_column :booth_structures, :structure_id
-    remove_column :booth_structures, :inventory_capcity
-    remove_column :booth_structures, :item_price
+    remove_column :booths, :structure_id
+    remove_column :booths, :inventory_capcity
+    remove_column :booths, :item_price
   end
 end
